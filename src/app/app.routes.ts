50e33501f661297aee8b61df7401ad1c0d1ea176
r0zh <r0zh017@proton.me>
2024-05-09 11:08:55 +0200

    Add routes for home, gallery, and community components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CommunityComponent } from './community/community.component';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'community', component: CommunityComponent },
+];
