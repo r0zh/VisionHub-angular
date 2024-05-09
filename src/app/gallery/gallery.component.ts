63991ced23cdc92d3062bd50c862487e0f647f89
r0zh <r0zh017@proton.me>
2024-05-09 11:10:10 +0200

    Add gallery component with model rendering

---
+++ src/app/gallery/gallery.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ModelComponent } from '../model/model.component';
+
+@Component({
+  selector: 'app-gallery',
+  standalone: true,
+  imports: [ModelComponent],
+  templateUrl: './gallery.component.html',
+  styleUrl: './gallery.component.css'
+})
+export class GalleryComponent {
+  modelUrls: string[] = [];
+  //modelUrls: string[] = ['assets/models/model1/scene.gltf', 'assets/models/model2/scene.gltf'];
+}
