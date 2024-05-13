871e91b889d3a05d702e94080e0b6c9a814c6160
r0zh <r0zh017@proton.me>
2024-05-13 07:20:47 +0200

    Add login and register routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,16 @@ import { Routes } from '@angular/router';
 import { CommunityComponent } from './community/community.component';
 import { HomeComponent } from './home/home.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'gallery', component: GalleryComponent },
     { path: 'community', component: CommunityComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'login', redirectTo: '/auth/login', pathMatch: 'full' },
+    { path: 'register', redirectTo: '/auth/register', pathMatch: 'full' },
+    { path: 'auth/login', component: LoginComponent },
+    { path: 'auth/register', component: RegisterComponent }
 ];
