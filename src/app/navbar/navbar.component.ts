4e2c3443880bcd40590a93d44c47df2ab0390a9a
Roberto Sánchez Martín <32245814+r0zh@users.noreply.github.com>
2024-05-15 15:00:25 +0000

    chore: Update navbar component with login and profile functionality

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,36 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { MenubarModule } from 'primeng/menubar';
-import { MenuItem } from 'primeng/api';
-import { ButtonModule } from 'primeng/button';
+import { Component, OnInit } from "@angular/core";
+import { MenubarModule } from "primeng/menubar";
+import { MenuItem } from "primeng/api";
+import { ButtonModule } from "primeng/button";
+import { AuthService } from "../auth/auth.service";
+import { SplitButtonModule } from "primeng/splitbutton";
 
 @Component({
-  selector: 'app-navbar',
+  selector: "app-navbar",
   standalone: true,
-  imports: [MenubarModule, ButtonModule],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  imports: [MenubarModule, ButtonModule, SplitButtonModule],
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.css",
 })
 export class NavbarComponent implements OnInit {
+  constructor(private authService: AuthService) {}
   items: MenuItem[] | undefined;
+  profileItems: MenuItem[] | undefined;
 
   ngOnInit() {
     this.items = [
       {
-        label: 'Home',
-        icon: 'pi pi-home',
-        route: '/home'
+        label: "Home",
+        icon: "pi pi-home",
+        route: "/home",
       },
       {
-        label: 'Gallery',
-        icon: 'pi pi-image',
-        route: '/gallery'
+        label: "Gallery",
+        icon: "pi pi-image",
+        route: "/gallery",
       },
       {
-        label: 'Community',
-        icon: 'pi pi-star',
-        route: '/community'
-      }
-    ]
+        label: "Community",
+        icon: "pi pi-star",
+        route: "/community",
+      },
+    ];
+
+    this.profileItems = [
+      {
+        label: "Profile",
+        icon: "pi pi-user",
+        route: "/user/profile",
+      },
+      {
+        label: "Logout",
+        icon: "pi pi-sign-out",
+        command: () => {
+          this.authService.logout();
+        },
+      },
+    ];
   }
 
+  isLoggedIn() {
+    return this.authService.isLoggedIn();
+  }
 }
