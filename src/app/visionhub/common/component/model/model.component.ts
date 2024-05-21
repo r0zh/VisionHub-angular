3565b7531e241dd2892de1fda22f280d40ded598
Roberto Sánchez Martín <32245814+r0zh@users.noreply.github.com>
2024-05-21 16:55:51 +0000

    implement image component and gallery

--- src/app/visionhub/common/component/model/model.component.ts
+++ src/app/visionhub/common/component/model/model.component.ts
@@ -1,59 +1,16 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef, Input } from "@angular/core";
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { Image } from "../../model/image";
+import { CardModule } from "primeng/card";
 
 @Component({
   selector: "app-model",
   standalone: true,
-  imports: [],
-  template: `<div #rendererContainer></div>`,
+  imports: [CardModule],
+  templateUrl: "./model.component.html",
   styleUrl: "./model.component.css",
 })
-export class ModelComponent implements OnInit {
-  @ViewChild("rendererContainer")
-  rendererContainer!: ElementRef;
-
-  private scene: THREE.Scene;
-  private camera: THREE.PerspectiveCamera;
-  private renderer: THREE.WebGLRenderer;
-  private loader: GLTFLoader;
-
-  constructor() {
-    this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    this.loader = new GLTFLoader();
-
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-    this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
-  }
-
-  ngOnInit(): void {
-    this.loadModels(["assets/models/model1/scene.gltf", "assets/models/model2/scene.gltf"]);
-  }
-
-  private async loadModels(modelUrls: string[]): Promise<void> {
-    await Promise.all(modelUrls.map((url) => this.loadModel(url)));
-  }
-
-  private async loadModel(url: string): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.loader.load(
-        url,
-        (gltf) => {
-          gltf.scene.scale.set(0.1, 0.1, 0.1); // Adjust scale as needed
-          this.scene.add(gltf.scene);
-          this.camera.position.z = 5; // Position the camera
-          this.render();
-          resolve();
-        },
-        undefined,
-        reject
-      );
-    });
-  }
-
-  private render(): void {
-    this.renderer.render(this.scene, this.camera);
-  }
+export class ModelComponent {
+  @Input() modelObject: Image | undefined;
 }
--- src/app/visionhub/pages/community/community.component.ts
+++ src/app/visionhub/pages/community/community.component.ts
@@ -1,10 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../auth/services/auth.service";
+import { ImageService } from "../../common/service/image/image.service";
+import { Image } from "../../common/model/image";
+import { ModelComponent } from "../../common/component/model/model.component";
 
 @Component({
-  selector: "app-community",
-  standalone: true,
-  imports: [],
-  templateUrl: "./community.component.html",
-  styleUrl: "./community.component.css",
+    selector: "app-community",
+    standalone: true,
+    templateUrl: "./community.component.html",
+    styleUrl: "./community.component.css",
+    imports: [ModelComponent]
 })
-export class CommunityComponent {}
+export class CommunityComponent implements OnInit {
+  constructor(private authService: AuthService, private imageService: ImageService) {}
+  images: Image[] | undefined;
+
+  ngOnInit(): void {
+    this.getImages();
+  }
+
+  getImages() {
+    let images = this.imageService.getAllImages().subscribe((images) => {
+      this.images = images;
+    });
+  }
+}
