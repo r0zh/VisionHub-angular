badba7c4d59d0547d770ce05db380e077f216908
r0zh <r0zh017@proton.me>
2024-06-12 18:36:32 +0000

    add model inspection for the model component

--- src/app/visionhub/common/component/model/model.component.ts
+++ src/app/visionhub/common/component/model/model.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit, ViewChild, ElementRef, Input, AfterViewInit, model }
 import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { Image } from "../../model/image";
-import { CardModule } from "primeng/card";
 import { environment } from "../../../../../environments/environment";
 import { ImageService } from "../../service/image/image.service";
 import { DomSanitizer } from "@angular/platform-browser";
 import { ProgressSpinnerModule } from "primeng/progressspinner";
 import { ThreeDModel } from "../../model/three_d_model";
 import { ThreeDModelService } from "../../service/image/three_d_model.service";
+import { PanelModule } from "primeng/panel";
+import { ModelViewerComponent } from "../model-viewer/model-viewer.component";
+import { DialogModule } from "primeng/dialog";
 
 @Component({
   selector: "app-model",
   standalone: true,
-  imports: [CardModule, ProgressSpinnerModule],
+  imports: [ProgressSpinnerModule, PanelModule, ModelViewerComponent, DialogModule],
   templateUrl: "./model.component.html",
   styleUrl: "./model.component.css",
 })
@@ -21,6 +23,12 @@ export class ModelComponent implements AfterViewInit {
   @Input() modelObject: ThreeDModel | undefined;
   @ViewChild("lazyImage") lazyImage!: ElementRef<HTMLImageElement>;
   sourceLoaded = false;
+  modelPath = "";
+
+  modalOpen = false;
+
+  windowWidth = window.innerWidth;
+  windowHeight = window.innerHeight;
 
   constructor(private threeDModelService: ThreeDModelService, private sanitizer: DomSanitizer) {}
 
@@ -37,6 +45,14 @@ export class ModelComponent implements AfterViewInit {
         };
       });
     }
+  }
+
+  viewModel() {
+    console.log(this.modelObject);
+    this.threeDModelService.getThreeDModel(this.modelObject!.id).subscribe((model) => {
+      this.modelPath = URL.createObjectURL(model);
+      this.modalOpen = true;
+    });
   }
 
   // display gif of model on hover
