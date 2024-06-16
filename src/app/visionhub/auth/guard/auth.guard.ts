653b094df5045110b3f32fad20dbfd1f51f54d46
r0zh <r0zh017@proton.me>
2024-06-16 14:03:35 +0000

    refactor auth.guard to use authService.checkToken for token validation and navigation

--- src/app/visionhub/auth/guard/auth.guard.ts
+++ src/app/visionhub/auth/guard/auth.guard.ts
@@ -1,15 +1,22 @@
-import { inject } from '@angular/core';
-import { AuthService } from './../services/auth.service';
-import { CanActivateFn, Router } from '@angular/router';
+import { inject } from "@angular/core";
+import { AuthService } from "./../services/auth.service";
+import { CanActivateFn, Router } from "@angular/router";
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
-  const router = inject(Router); 
+  const router = inject(Router);
 
-  if (!authService.isLoggedIn()) {
-    router.navigate(['/login']);
-    return false;
-  }
-  
+  authService.checkToken().subscribe({
+    next: (response) => {
+      console.log("Token is valid");
+      return true;
+    },
+    error: (error) => {
+      console.log("Token is invalid");
+      router.navigate(["/login"]);
+      return false;
+    },
+  });
+
   return true;
 };
