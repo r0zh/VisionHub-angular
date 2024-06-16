adaf2a2219cf7929d0db1375e55731fc8c256522
r0zh <r0zh017@proton.me>
2024-06-16 12:44:13 +0000

    improve authService error handling

--- src/app/visionhub/auth/services/auth.service.ts
+++ src/app/visionhub/auth/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { isPlatformBrowser } from "@angular/common";
-import { HttpClient } from "@angular/common/http";
-import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Inject, Injectable, PLATFORM_ID, WritableSignal, signal } from "@angular/core";
 import { MessageService } from "primeng/api";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, Observable, catchError, throwError } from "rxjs";
 import { environment } from "../../../../environments/environment";
 import { AuthResponse } from "../model/auth-response";
 import { User } from "../../common/model/user";
 import { Router } from "@angular/router";
+import { Console } from "console";
 
 @Injectable({
   providedIn: "root",
@@ -14,15 +15,25 @@ import { Router } from "@angular/router";
 export class AuthService {
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false); // Default to false until authenticated
   public isAuthenticated$: Observable<boolean> = this.isAuthenticatedSubject.asObservable();
-
+  private user!: WritableSignal<User>;
   constructor(
     @Inject(PLATFORM_ID) private platformId: object,
     private http: HttpClient,
     private messageService: MessageService,
     private router: Router
   ) {
-    if (this.isLoggedIn()) {
-      this.isAuthenticatedSubject.next(true);
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkToken().subscribe({
+        next: (response) => {
+          if (response.status === true) {
+            this.user = signal(response.user);
+            this.isAuthenticatedSubject.next(true);
+          }
+        },
+        error: (error) => {
+          this.handleError(error);
+        },
+      });
     }
   }
 
@@ -30,8 +41,8 @@ export class AuthService {
     this.http.post<AuthResponse>(`${environment.apiUrl}/auth/login`, { email, password }).subscribe({
       next: (response) => {
         this.handleLoginSuccess(response.token, rememberMe);
+        this.user = signal(response.user);
         this.messageService.add({ severity: "success", detail: "Logged in successfully" });
-        //redirect to community
         this.router.navigate(["/community"]);
       },
       error: (error) => {
@@ -74,8 +85,39 @@ export class AuthService {
 
   public isLoggedIn(): boolean {
     if (isPlatformBrowser(this.platformId)) {
+      console.log(!!localStorage.getItem("token") || !!sessionStorage.getItem("token"));
       return !!localStorage.getItem("token") || !!sessionStorage.getItem("token");
     }
     return false;
+  }
+
+  public checkToken() {
+    console.log(this.getToken());
+    const options = {
+      headers: new HttpHeaders({
+        Authorization: "Bearer " + this.getToken(),
+      }),
+    };
+
+    return this.http.get<AuthResponse>(`${environment.apiUrl}/auth/checkToken`, options);
+  }
+
+  public getCurrentUser(): WritableSignal<User> {
+    return this.user;
+  }
+
+  private handleError(error: any) {
+    if (error.status === 401) {
+      this.messageService.add({ severity: "error", summary: "Error", detail: "Unathorized" });
+      this.router.navigate(["/login"]);
+    }
+    if (error.status === 403) {
+      this.messageService.add({ severity: "error", summary: "Error", detail: "Forbidden" });
+    }
+    if (error.status === 500) {
+      this.messageService.add({ severity: "error", summary: "Error", detail: "Internal server error" });
+    }
+    this.router.navigate(["/login"]);
+    this.isAuthenticatedSubject.next(false);
   }
 }
