d15916b38fbb2bf937ab1f304e38748b943d28a5
r0zh <r0zh017@proton.me>
2024-06-16 13:15:07 +0000

    refactor navbar component to improve user authentication and navigation

--- src/app/visionhub/common/component/navbar/navbar.component.ts
+++ src/app/visionhub/common/component/navbar/navbar.component.ts
@@ -1,4 +1,14 @@
-import { ChangeDetectorRef, Component, OnInit, afterNextRender, afterRender } from "@angular/core";
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+  WritableSignal,
+  afterNextRender,
+  afterRender,
+} from "@angular/core";
 import { MenuItem, MessageService } from "primeng/api";
 import { ButtonModule } from "primeng/button";
 import { MenubarModule } from "primeng/menubar";
@@ -8,6 +18,8 @@ import { environment } from "../../../../../environments/environment";
 import { AuthService } from "../../../auth/services/auth.service";
 import { VISIONHUB_PATHES } from "../../../properties/properties";
 import { User } from "../../model/user";
+import { UserService } from "../../service/user.service";
+import { isPlatformBrowser } from "@angular/common";
 
 @Component({
   selector: "app-navbar",
@@ -16,86 +28,94 @@ import { User } from "../../model/user";
   templateUrl: "./navbar.component.html",
   styleUrl: "./navbar.component.css",
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements AfterViewInit {
   items: MenuItem[] | undefined;
   profileItems: MenuItem[] | undefined;
   loginPath = VISIONHUB_PATHES.LOGIN;
   registerPath = VISIONHUB_PATHES.REGISTER;
-  private authSubscription?: Subscription; // To hold the subscription
-  private user?: User;
+  profile: User | undefined;
+  isAuthenticated = false;
 
   constructor(
+    @Inject(PLATFORM_ID) private platformId: object,
     private authService: AuthService,
     private messageService: MessageService,
     private cdr: ChangeDetectorRef // Inject ChangeDetectorRef
-  ) { }
+  ) {}
 
-  ngOnInit() {
-    this.authSubscription = this.authService.isAuthenticated$.subscribe((isAuthenticated) => {
-      this.updateMenuItems(isAuthenticated);
-      this.cdr.markForCheck(); // Mark for check after updating menu items
-    });
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.authService.isAuthenticated$.subscribe((isAuthenticated: boolean) => {
+        this.isAuthenticated = isAuthenticated;
+        this.updateMenuItems(isAuthenticated);
+        if (isAuthenticated) {
+          this.authService.getCurrentUser().subscribe((user: User) => {
+            this.profile = user;
+          });
+        } else {
+          this.profile = undefined;
+        }
+        this.cdr.markForCheck();
+        this.cdr.detectChanges();
+      });
+    }
   }
 
   updateMenuItems(isLoggedIn: boolean) {
     this.items = isLoggedIn
       ? [
-        {
-          label: "Home",
-          icon: "pi pi-home",
-          route: VISIONHUB_PATHES.HOME,
-        },
-        {
-          label: "Generator",
-          icon: "pi pi-cog",
-          route: VISIONHUB_PATHES.GENERATOR,
-        },
-        {
-          label: "Gallery",
-          icon: "pi pi-image",
-          route: VISIONHUB_PATHES.GALLERY,
-        },
-        {
-          label: "Community",
-          icon: "pi pi-star",
-          route: VISIONHUB_PATHES.COMMUNITY,
-        },
-      ]
+          {
+            label: "Home",
+            icon: "pi pi-home",
+            route: VISIONHUB_PATHES.HOME,
+          },
+          {
+            label: "Generator",
+            icon: "pi pi-cog",
+            route: VISIONHUB_PATHES.GENERATOR,
+          },
+          {
+            label: "Gallery",
+            icon: "pi pi-image",
+            route: VISIONHUB_PATHES.GALLERY,
+          },
+          {
+            label: "Community",
+            icon: "pi pi-star",
+            route: VISIONHUB_PATHES.COMMUNITY,
+          },
+        ]
       : [
-        {
-          label: "Home",
-          icon: "pi pi-home",
-          route: VISIONHUB_PATHES.HOME,
-        },
-      ];
+          {
+            label: "Home",
+            icon: "pi pi-home",
+            route: VISIONHUB_PATHES.HOME,
+          },
+        ];
 
     this.profileItems = isLoggedIn
       ? [
-        {
-          label: "Profile",
-          icon: "pi pi-user",
-          command: () => {
-            this.navigateToProfile();
+          {
+            label: this.profile?.name,
+            icon: "pi pi-user",
+            command: () => {
+              this.navigateToProfile();
+            },
           },
-        },
-        {
-          label: "Logout",
-          icon: "pi pi-sign-out",
-          command: () => {
-            this.authService.logout();
-            this.messageService.add({
-              severity: "success",
-              detail: "Logged out successfully",
-            });
-            this.cdr.detectChanges();
+          {
+            label: "Logout",
+            icon: "pi pi-sign-out",
+            command: () => {
+              this.authService.logout();
+              this.messageService.add({
+                severity: "success",
+                detail: "Logged out successfully",
+              });
+              this.cdr.detectChanges();
+            },
           },
-        },
-      ]
+        ]
       : [];
-  }
-
-  isLoggedIn() {
-    return this.authService.isLoggedIn();
   }
 
   // send to `${enviroment.webUrl}/profile`
