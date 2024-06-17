8cdf3583c2eb860520a866a86c9b6a7e2cf363fc
r0zh <r0zh017@proton.me>
2024-06-17 10:11:15 +0000

    refactor: Update flaskUrl in environment files to use ngrok URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   apiUrl: "http://laravelloadbalancer-1094644412.us-east-1.elb.amazonaws.com/api",
   webUrl: "http://laravelloadbalancer-1094644412.us-east-1.elb.amazonaws.com",
-  flaskUrl: "https://localhost:5000/",
+  flaskUrl: "https://7b33-34-74-68-159.ngrok-free.app",
 };
